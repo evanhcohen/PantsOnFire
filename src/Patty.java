bd2b573fcb31ba825b77ad4b44e770db9dad3583
evanhcohen <evancohen19@gmail.com>
2020-05-28 15:25:28 -0400

    override methods

--- src/Patty.java
+++ src/Patty.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -22,4 +21,23 @@ public class Patty extends JPanel implements ActionListener, KeyListener
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
