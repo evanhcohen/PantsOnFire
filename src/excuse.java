4b28b5a00ff7923d59bafecd53d536a638c1ff27
evanhcohen <evancohen19@gmail.com>
2020-05-27 21:43:56 -0400

    i made some edits

--- src/excuse.java
+++ src/excuse.java
@@ -3,5 +3,7 @@ public class excuse
     public static void main (String[] args)
     {
         System.out.println("some edits");
+        System.out.println("mega edits");
+        System.out.println("even more");
     }
 }
