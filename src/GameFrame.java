37b743147a21e832c8d452d046fab48093d0ba21
evanhcohen <evancohen19@gmail.com>
2020-05-28 10:11:19 -0400

    update GameFrame and runner

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,3 +1,30 @@
-public class GameFrame
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame
 {
+    private JFrame frame;
+    private SamplePanel panel; // modify this to a different panel
+
+    // constructor
+    public GameFrame()
+    {
+        // creates the frame
+        frame = new JFrame("Pants on Fyre");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // creates the panel
+        panel = new SamplePanel(); // modify this later
+        panel.setPreferredSize(new Dimension(500,300));
+
+        // add the panel to the frame
+        frame.getContentPane().add(panel);
+    }
+
+    // shows the frame
+    public void display()
+    {
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/PantsOnFyreRunner.java
+++ src/PantsOnFyreRunner.java
@@ -1,3 +1,8 @@
 public class PantsOnFyreRunner
 {
+    public static void main (String[] args)
+    {
+        GameFrame g = new GameFrame();
+        g.display();
+    }
 }
