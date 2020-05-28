7189a7547c3469ab8963e74d74329e29b54f4d34
evanhcohen <evancohen19@gmail.com>
2020-05-27 21:57:45 -0400

    we are coding added

--- src/test.java
+++ src/test.java
@@ -2,6 +2,6 @@ public class test
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! How are you?");
+        System.out.println("Hello World! How are you? We are coding!");
     }
 }
