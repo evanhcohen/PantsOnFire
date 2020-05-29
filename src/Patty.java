0ca774c56850789f7537bca72dd6b452114f13f2
AnthonySarro <asarro623@gmail.com>
2020-05-29 14:57:57 -0400

    updated patty class

--- src/Patty.java
+++ src/Patty.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 public class Patty extends JComponent
+
 {
     // instance variables
     private int lives;
