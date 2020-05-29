ea934adc6fd8201f8e0875df0f4f0e3472ce541f
AnthonySarro <asarro623@gmail.com>
2020-05-29 14:53:39 -0400

    updated patty class

--- src/Patty.java
+++ src/Patty.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-public class Patty extends JPanel implements ActionListener, KeyListener
+public class Patty extends JComponent
 {
     // instance variables
     private int lives;
@@ -21,23 +21,5 @@ public class Patty extends JPanel implements ActionListener, KeyListener
     }
 
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
