db4a82841c04ed9747484c203abd8b868362aeb5
evanhcohen <evancohen19@gmail.com>
2020-06-05 14:02:13 -0400

    minor changes --> simplifying code

--- src/MenuFrame.java
+++ src/MenuFrame.java
@@ -5,7 +5,6 @@ public class MenuFrame extends JFrame
 {
     private static JFrame frame;
     private static MenuPanel menu;
-    private static InstructionsPanel instructions;
     private static int panelNum = 0;
 
     public MenuFrame()
@@ -16,10 +15,8 @@ public class MenuFrame extends JFrame
 
         // creates panel
         menu = new MenuPanel();
-        instructions = new InstructionsPanel();
 
         menu.setPreferredSize(new Dimension(1100,700));
-        instructions.setPreferredSize(new Dimension(1100,700));
 
         // add menu panel to the frame
         frame.getContentPane().add(menu);
@@ -43,11 +40,6 @@ public class MenuFrame extends JFrame
         {
             frame.getContentPane().removeAll();
             frame.getContentPane().add(menu);
-        }
-        else if(panelNum == 2)
-        {
-            frame.getContentPane().removeAll();
-            frame.getContentPane().add(instructions);
         }
         display();
     }
