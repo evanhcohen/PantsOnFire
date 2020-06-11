8309def60f6b0a32ffa580e6f2fc71bcb522539b
evanhcohen <evancohen19@gmail.com>
2020-06-10 23:23:46 -0400

    refreshed buttons

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -9,38 +9,47 @@ import java.io.IOException;
 
 public class MenuPanel extends JPanel implements ActionListener, KeyListener
 {
-    private JButton start;
+    private JButton play;
     private JButton instructions;
 
-    private Image play;
+    private Image playImage;
     private int playSize = 15;
 
     public MenuPanel()
     {
+
         // set up images
         try {
-            play = ImageIO.read(getClass().getResource("images/play.png")).getScaledInstance(playSize,playSize, Image.SCALE_SMOOTH);
+            playImage = ImageIO.read(getClass().getResource("images/play.png")).getScaledInstance(playSize,playSize, Image.SCALE_SMOOTH);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        play = new JButton("Play", new ImageIcon(playImage));
+        play.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK, 3),
+                BorderFactory.createEmptyBorder(1, 5, 1, 5)));
+        play.setContentAreaFilled(false);
+        play.setFocusPainted(false);
+        play.setOpaque(false);
+        play.addActionListener(this);
+        play.setActionCommand("Start");
+        play.setBounds(0,300, 0, 0);
+        add(play);
 
-        start = new JButton("START", new ImageIcon(play));
-        start.setMargin(new Insets(0, 0, 0, 0));
-        start.setBorder(null);
-        start.setBorderPainted(false);
-        start.setContentAreaFilled(false);
-        start.setFocusPainted(false);
-        start.setOpaque(false);
-        start.addActionListener(this);
-        start.setActionCommand("Start");
-        add(start);
-
-        instructions = new JButton("HOW TO PLAY");
+        instructions = new JButton("Instructions");
+        instructions.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK, 3),
+                BorderFactory.createEmptyBorder(1, 5, 1, 5)));
+        instructions.setContentAreaFilled(false);
+        instructions.setFocusPainted(false);
+        instructions.setOpaque(false);
         instructions.addActionListener(this);
         instructions.setActionCommand("Instructions");
+        instructions.setBounds(0,300, 0, 0);
         add(instructions);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
