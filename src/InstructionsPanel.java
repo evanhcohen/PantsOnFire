fca80ac56012b1e8f4531272cb8416f6049bf43b
evanhcohen <evancohen19@gmail.com>
2020-06-11 00:57:42 -0400

    more button revamp

--- src/InstructionsPanel.java
+++ src/InstructionsPanel.java
@@ -1,25 +1,45 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 public class InstructionsPanel extends JPanel implements ActionListener, KeyListener
 {
     private JButton menu;
+    private Image menuImage;
+    private int menuSize = 15;
 
     public InstructionsPanel()
     {
-        menu = new JButton("MENU");
+        // set up images
+        try {
+            menuImage = ImageIO.read(getClass().getResource("images/menu.png")).getScaledInstance(menuSize,menuSize, Image.SCALE_SMOOTH);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        menu = new JButton("Menu", new ImageIcon(menuImage));
+        menu.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK, 3),
+                BorderFactory.createEmptyBorder(1, 5, 1, 5)));
+        menu.setContentAreaFilled(false);
+        menu.setFocusPainted(false);
+        menu.setOpaque(false);
         menu.addActionListener(this);
-        menu.setActionCommand("Menu");
+        menu.setActionCommand("menu");
+        menu.setBounds(0,300, 0, 0);
         add(menu);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String action = e.getActionCommand();
-        if (action.equals("Menu"))
+        if (action.equals("menu"))
         {
             MenuFrame.changePanel(1);
             GameFrame.changePanel(1);
