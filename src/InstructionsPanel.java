c6f9535d8accc5a0382df8605554a14ece983f87
evanhcohen <evancohen19@gmail.com>
2020-06-11 20:56:43 -0400

    instructions screen appearance edits

--- src/InstructionsPanel.java
+++ src/InstructionsPanel.java
@@ -17,6 +17,12 @@ public class InstructionsPanel extends JPanel implements ActionListener, KeyList
     private JLabel inst;
     private Dimension instDim;
 
+    private Image exampleImg;
+    private JLabel example;
+    private int exH = (int) (460 * .75);
+    private int exW = (int) (557 * .75);
+    private Dimension exDim;
+
     public InstructionsPanel()
     {
         setLayout(null);
@@ -24,7 +30,7 @@ public class InstructionsPanel extends JPanel implements ActionListener, KeyList
         // set up images
         try {
             menuImage = ImageIO.read(getClass().getResource("images/menu.png")).getScaledInstance(menuSize,menuSize, Image.SCALE_SMOOTH);
-
+            exampleImg = ImageIO.read(getClass().getResource("images/example.jpg")).getScaledInstance(exW,exH, Image.SCALE_SMOOTH);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,15 +56,22 @@ public class InstructionsPanel extends JPanel implements ActionListener, KeyList
         inst.setText("<html> " +
                         "Thank you for answering our call!<br>" +
                         "The city is on fire and <b>you</b> are the only one who can stop it!<br>" +
-                        "Use the arrow keys to move around.<br>" +
-                        "Make sure to collect the fire extinguisher first.<br>" +
-                        "Now, it's your job to save the city. Good luck!" +
-                     "</html>");
-        inst.setFont(new Font("Arial", Font.PLAIN, 20));
-        instDim = inst.getPreferredSize();
-        inst.setBounds(0,MainFrame.height/2 - instDim.height/2, 1500, instDim.height);
+                        "Be sure to collect the fire extinguisher before getting to the fire!<br>" +
+                        "Now, it's your job to save the city. Good luck!<br><br>" +
+                        "Use the arrow keys to move around." +
 
+                        "</html>");
+        inst.setFont(new Font("Arial", Font.PLAIN, 25));
+        instDim = inst.getPreferredSize();
+        inst.setBounds(200,MainFrame.height/2 - instDim.height/2, 1500, instDim.height);
         add(inst);
+
+        example = new JLabel();
+        example.setIcon(new ImageIcon(exampleImg));
+        exDim = example.getPreferredSize();
+        example.setBounds(MainFrame.width/2 + 200,MainFrame.height/2 - exDim.height/2, 1500, exDim.height);
+        add(example);
+
     }
 
     @Override
