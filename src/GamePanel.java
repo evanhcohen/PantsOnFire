5b4a5ecde35afff1e153b1deccb94f13f00a8311
evanhcohen <evancohen19@gmail.com>
2020-06-15 10:51:27 -0400

    minor edit

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -100,8 +100,8 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener
         setLayout(null);
 
         // set up player object
-        p = new Patty(rel/31, rY(360), rY(400)-rY(360), rY(400)-rY(360));
-//        p = new Patty(675, 124-50,50,50); // used for testing fire & fire extinguisher
+//        p = new Patty(rel/31, rY(360), rY(400)-rY(360), rY(400)-rY(360));
+        p = new Patty(675, 124-40,40,40); // used for testing fire & fire extinguisher
         p.drawImg();
 
         //sets up fire
@@ -131,7 +131,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener
         platform8.drawImg();
         platform9 = new Platform(rX(2.276), rY(124));
         platform9.drawImg();
-        platform10 = new Platform(rX(1.90), rY(100));
+        platform10 = new Platform(rX(1.87), rY(100));
         platform10.drawImg();
         platform11 = new Platform(rX(1.484), rY(92));
         platform11.drawImg();
